b404c9d29bf630e8d60111101a751f2279946cea
Xiaohua Zhang <zhang30@llnl.gov>
2018-07-09 15:19:41 -0700

    Modify the example

--- examples/simulateAmber.py
+++ examples/simulateAmber.py
@@ -2,12 +2,15 @@ from simtk.openmm.app import *
 from simtk.openmm import *
 from simtk.unit import *
 from sys import stdout
+import time
 
+start = time.time()
 prmtop = AmberPrmtopFile('input.prmtop')
 inpcrd = AmberInpcrdFile('input.inpcrd')
 system = prmtop.createSystem(nonbondedMethod=PME, nonbondedCutoff=1*nanometer, constraints=HBonds)
 integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
-simulation = Simulation(prmtop.topology, system, integrator)
+platform = Platform.getPlatformByName('CUDA')
+simulation = Simulation(prmtop.topology, system, integrator, platform)
 simulation.context.setPositions(inpcrd.positions)
 if inpcrd.boxVectors is not None:
     simulation.context.setPeriodicBoxVectors(*inpcrd.boxVectors)
@@ -15,3 +18,6 @@ simulation.minimizeEnergy()
 simulation.reporters.append(PDBReporter('output.pdb', 1000))
 simulation.reporters.append(StateDataReporter(stdout, 1000, step=True, potentialEnergy=True, temperature=True))
 simulation.step(10000)
+
+end = time.time()
+print(end - start)
